8c871d7898f8b308405ddfef46e19f9e5c8b0529
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-06-13 14:47:49 -0600

    Abstract paths and add server linting

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,43 @@
+// core gulp stuff
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+
+    // plugins used by multiple tasks
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
+
+var paths = {
+    server: {
+        js: ['./Gulpfile.js', './app.js', './routes/**/*.js']
+    },
+    client: {
+        js: ['./assets/**/*.js'],
+        styles: ['../assets/styl/main.styl']
+    }
+};
 
 gulp.task('stylus', function () {
     var stylus = require('gulp-stylus');
 
-    return gulp.src('./assets/styl/main.styl')
+    return gulp.src(paths.client.styles)
         .pipe(stylus())
         .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('js', function () {
-    var uglify  = require('gulp-uglify'),
-        jshint  = require('gulp-jshint'),
-        stylish = require('jshint-stylish');
+gulp.task('js:client', function () {
+    var uglify  = require('gulp-uglify');
 
-    return gulp.src('./assets/js/**/*.js')
+    return gulp.src(paths.client.js)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(uglify())
         .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('default', ['stylus', 'js']);
+gulp.task('js:server', function () {
+    return gulp.src(paths.server.js)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('default', ['stylus', 'js:client', 'js:server']);
