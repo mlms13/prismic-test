cfe4d0a2e4a3ad3dae2a1f73317bb52446f9fe60
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-06-13 16:26:21 -0600

    Reorganize app.js
    Because I’m picky about spacing…

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express      = require('express'),
+    app          = express(),
+    path         = require('path'),
+    router       = express.Router(),
+
+    // middleware
+    favicon      = require('static-favicon'),
+    logger       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
